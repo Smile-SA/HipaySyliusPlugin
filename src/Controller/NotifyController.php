793de40198c1e71114280ca8bf6997b5a9ee96f8
Georges Limouzy <georges.limouzy@smile.fr>
2021-09-27 16:52:41 +0200

    Added legal licence + strict type headers

--- src/Controller/NotifyController.php
+++ src/Controller/NotifyController.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is part of the HipaySyliusPlugin
+ *
+ * (c) Smile <dirtech@smile.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Smile\HipaySyliusPlugin\Controller;
 
 use App\Entity\Payment\Payment;
--- src/Form/Type/HipayConfigurationType.php
+++ src/Form/Type/HipayConfigurationType.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the HipaySyliusPlugin
+ *
+ * (c) Smile <dirtech@smile.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 declare(strict_types=1);
 
@@ -6,7 +14,6 @@ namespace Smile\HipaySyliusPlugin\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class HipayConfigurationType extends AbstractType
